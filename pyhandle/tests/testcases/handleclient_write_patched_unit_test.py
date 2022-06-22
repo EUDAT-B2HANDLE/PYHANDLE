3f745809d92fd884caeed5ef74a202e56020f450
Themis Zamani <themiszamani@gmail.com>
2022-06-22 17:24:16 +0300

    Update tests for all python versions

--- pyhandle/tests/testcases/handleclient_write_patched_unit_test.py
+++ pyhandle/tests/testcases/handleclient_write_patched_unit_test.py
@@ -342,7 +342,9 @@ class RESTHandleClientWriteaccessPatchedTestCase(unittest.TestCase):
         #expected_payload = {"values": [{"index": 100, "type": "HS_ADMIN", "data": {"value": {"index": 300, "handle": "handle/owner", "permissions": "011111110011"}, "format": "admin"}}, {"index": 1, "type": "URL", "data": "http://foo.bar"}, {"index": 2, "type": "CHECKSUM", "data": "123456"}, {"index": 3, "type": "FOO", "data": "foo"}, {"index": 4, "type": "BAR", "data": "bar"}, {"index": 5, "type": "10320/LOC", "data": "<locations><location href=\"http://bar.bar\" id=\"0\" /><location href=\"http://foo.foo\" id=\"1\" /></locations>"}]}
         # Changed order/index: old one
         #expected_payload = {"values": [{"index": 100, "type": "HS_ADMIN", "data": {"value": {"index": 300, "handle": "handle/owner", "permissions": "011111110011"}, "format": "admin"}}, {"index": 1, "type": "URL", "data": "http://foo.bar"}, {"index": 4, "type": "CHECKSUM", "data": "123456"}, {"index": 2, "type": "FOO", "data": "foo"}, {"index": 3, "type": "BAR", "data": "bar"}]}
-        if (sys.version_info.major == 3 and sys.version_info.minor >= 5):
+        if (sys.version_info.major == 3 and sys.version_info.minor == 5):
+            expected_payload = {"values": [{"data": {"format": "admin", "value": {"handle": "handle/owner", "index": 300, "permissions": "011111110011"}}, "type": "HS_ADMIN", "index": 100}, {"data": "foo", "type": "FOO", "index": 2}, {"data": "bar", "type": "BAR", "index": 3}, {"data": "http://foo.bar", "type": "URL", "index": 1}, {"data": "123456", "type": "CHECKSUM", "index": 4}]}
+        elif (sys.version_info.major == 3 and sys.version_info.minor > 5):
             expected_payload = {"values": [{"index": 100, "type": "HS_ADMIN", "data": {"value": {"index": 300, "handle": "handle/owner", "permissions": "011111110011"}, "format": "admin"}}, {"index": 2, "type": "FOO", "data": "foo"}, {"index": 3, "type": "BAR", "data": "bar"}, {"index": 1, "type": "URL", "data": "http://foo.bar"}, {"index": 4, "type": "CHECKSUM", "data": "123456"}]}
         else: 
             expected_payload = {"values": [{"index": 100, "type": "HS_ADMIN", "data": {"value": {"index": 300, "handle": "handle/owner", "permissions": "011111110011"}, "format": "admin"}}, {"index": 1, "type": "URL", "data": "http://foo.bar"}, {"index": 2, "type": "CHECKSUM", "data": "123456"}, {"index": 3, "type": "FOO", "data": "foo"}, {"index": 4, "type": "BAR", "data": "bar"}]}
@@ -735,7 +737,7 @@ class RESTHandleClientWriteaccessPatchedTestCase(unittest.TestCase):
         passed_payload, _ = self.get_payload_headers_from_mockresponse(putpatch)
         
         # Compare with expected payload:
-        if (sys.version_info.major == 3 and sys.version_info.minor > 6):
+        if (sys.version_info.major == 3 and sys.version_info.minor >= 5):
             expected_payload = {'values': [{'data': 'new100', 'index': 2, 'type': 'TEST100' }, {'data': 'new2', 'ttl': 86400, 'index': 2222, 'type': 'TEST2'}, {'data': 'new4', 'ttl': 86400, 'index': 4, 'type': 'TEST4'}, {'data': 'new101', 'index': 3, 'type': 'TEST101'}]}
         else: 
             expected_payload = {'values': [{'index': 2, 'type': 'TEST100', 'data': 'new100'}, {'index': 2222, 'ttl': 86400, 'type': 'TEST2', 'data': 'new2'}, {'index': 4, 'ttl': 86400, 'type': 'TEST4', 'data': 'new4'}, {'index': 3, 'type': 'TEST101', 'data': 'new101'}]}
