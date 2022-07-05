d53f3a05d00b2d0b346865793a25d691fd5f3333
Themis Zamani <themiszamani@gmail.com>
2022-07-06 00:53:35 +0300

    tests for python 3.10

--- pyhandle/tests/docker-entrypoint-310.sh
+++ pyhandle/tests/docker-entrypoint-310.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'coverage' ]; then
-  pytest -k 'not RESTHandleClientReadaccessTestCase and not  RESTHandleClientWriteaccessTestCase and not  RESTTHandleClientSearchTestCase' --cov-report xml:coverage.xml --cov=pyhandle --ignore=handleclient_read_integration_test.py
+  pytest -k 'not RESTHandleClientReadaccessTestCase and not  RESTHandleClientWriteaccessTestCase and not  RESTTHandleClientSearchTestCase' --cov-report xml:coverage.xml --cov=pyhandle
  
 else
   exec "$@"
