dad2969243247286a16b4b79f0db43cb5a5e707e
Themis Zamani <themiszamani@gmail.com>
2022-07-06 00:25:52 +0300

    tests for python 3.10

--- pyhandle/tests/docker-entrypoint-310.sh
+++ pyhandle/tests/docker-entrypoint-310.sh
@@ -2,7 +2,8 @@
 set -e
 
 if [ "$1" = 'coverage' ]; then
-  nosetests --with-xunit --xunit-testsuite-name=pyhandle --with-coverage --cover-erase --cover-package=pyhandle --cover-branches --cover-inclusive --cover-xml main_test_script.py
+  pytest --cov-report xml:coverage.xml --cov=pyhandle
+  #pytest --with-xunit --xunit-testsuite-name=pyhandle --with-coverage --cover-erase --cover-package=pyhandle --cover-branches --cover-inclusive --cover-xml main_test_script.py
 else
   exec "$@"
 fi
