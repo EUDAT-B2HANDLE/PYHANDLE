d236f7b4ed2fb2b1b74ae003692c9c44b6efb4b4
Themis Zamani <themiszamani@gmail.com>
2022-07-21 15:12:01 +0300

    Check python version dependencies

--- setup.py
+++ setup.py
@@ -93,7 +93,8 @@ setup(name='pyhandle',
           'datetime',
           'future',
           'six',
-          'pymysql==0.8.0'
+          'pymysql==0.8.0 ; python_version < "2.8.0"',
+          'pymysql; python_version < "3.10.0"',
 
       ],
       tests_require=test_dependencies,
